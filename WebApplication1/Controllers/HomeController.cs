94735071c82359604377d81a376e51bb6a8c7df3
Igor Bodenchyck <bodenchyck97@mail.ru>
2023-09-14 21:07:17 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Dto;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+
+    [Route("api/auth")]
+    public class HomeController : ControllerBase
+    {
+        private static readonly Dictionary<string, User> Users = new()
+        {
+            {
+                "admin@gmail.com", new User {Name = "admin", Email = "admin@gmail.com", Password = "lolkek"}
+            }
+        };
+
+        [HttpGet("users")]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return Users.Values;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDto dto)
+        {
+            if (Users.ContainsKey(dto.Email))
+            {
+                return BadRequest("User is exist already");
+            }
+
+            Users.Add(dto.Email, new User { Name = dto.Name, Email = dto.Email, Password = dto.Password });
+
+            return Ok();
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto dto )
+        {
+
+            if (!Users.TryGetValue(dto.Email, out var user))
+            {
+                return BadRequest("User is not exist");
+            }
+
+            return Ok(user);
+        }
+    }
+}
---
+++ WebApplication1/Dto/RegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Dto
+{
+    public class RegisterDto
+    {
+        [MinLength(5)]
+        [MaxLength(30)]
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [MinLength(5)]
+        [Required]
+        public string Email { get; set; }
+
+
+        [MaxLength(30)]
+        [Required]
+        public string Password { get; set; }         
+    }
+}
